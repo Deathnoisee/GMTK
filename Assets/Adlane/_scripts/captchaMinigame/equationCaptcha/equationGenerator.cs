0975611f579de0c37b3c7f04943364379472e5e1
Deathnoise <adlenalger2005@gmail.com>
2026-07-24 12:02:04 +0100

    MERFE CONFLICTS

--- Assets/Adlane/_scripts/captchaMinigame/equationCaptcha/equationGenerator.cs
+++ Assets/Adlane/_scripts/captchaMinigame/equationCaptcha/equationGenerator.cs
@@ -4,27 +4,5 @@ using UnityEngine;
 
 public static class EquationGenerator
 {
-    private static System.Random rng = new System.Random();
-
-    public static (string equation, int answer) GenerateEasy()
-    {
-        int answer = UnityEngine.Random.Range(5, 20);
-
-        int b = UnityEngine.Random.Range(1, 10);
-        int a = answer + b;
-
-        string equation = $"{a} - {b} = ?";
-        return (equation, answer);
-    }
-
-    public static (string equation, int answer) GenerateMedium()
-    {
-        int answer = UnityEngine.Random.Range(10, 50);
-
-        int b = UnityEngine.Random.Range(2, 9);
-        int a = answer * b;
-
-        string equation = $"{a} / {b} = ?";
-        return (equation, answer);
-    }
+    
 }
